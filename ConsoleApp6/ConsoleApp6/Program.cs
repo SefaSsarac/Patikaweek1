22e6c98cbd98bb3bffcac61e3aff943de2d88af5
Sefa Saraç <srcsfysf@gmail.com>
2024-07-16 13:34:43 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PatikaWeek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sefa Sarac");
+            Console.WriteLine("Patika'nın uzman kadrosu ile kendimi geliştirmek istiyorum!");
+        }
+
+    }
+}
